249953dc11a573aa6c8ea4f9a06ddad1c6e6fb7a
sakhalkark <sakhalkark@gmail.com>
2020-06-17 13:00:26 +0200

    Train Loader

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
-dataset_root    = "/media/kaustubh/New Volume/kaustubh_imp/datasets/BOLD_public/"
+# Path to dataset (str)
+dataset_root    = "/media/kaustubh/New_Volume/kaustubh_imp/datasets/BOLD_public/"
+
+# Learning Rate (float) <1.0
 learning_rate   = 0.1
+
+# Batch_size (int)
+batch_size      = 4
+
+# Number for frames for inputting the data (int)
+input_frames    = 32
