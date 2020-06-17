101089fbfc7f05e67eb1f63ad751256c0a18fc81
sakhalkark <sakhalkark@gmail.com>
2020-06-17 19:05:29 +0200

    TODO fix joints loader bug

--- dataloader/loader.py
+++ dataloader/loader.py
@@ -41,7 +41,55 @@ class BOLDTrainLoader(Dataset):
 
         if (vid_end - vid_start) > self.input_size:
             # Randomly select frames from the given video of input_size
-            arr         = random.sample(range(vid_start, vid_end), 
+            arr         = random.sample(range(vid_end - vid_start), 
+                                    self.input_size)
+            arr.sort()
+            vid_array   = vid_array[arr]
+            joints      = joints[arr]
+        else:
+            # Append the same video if the size is smaller than input_size
+            while vid_array.shape[0] < self.input_size:
+                vid_array = np.concatenate([vid_array, vid_array], axis = 0)
+                joints    = np.concatenate([joints, joints], axis = 0)
+
+        return vid_array, joints, emotions
+
+class BOLDValLoader(Dataset):
+    def __init__(self, dataroot = None, input_size = 32):
+        super().__init__()
+        # We have modified the orginal annotations to discard the 
+        # unrequired data and restructure it.
+        # Minimum frame size = 10
+        # Max frame size = 297
+        # Num vids with less than 32 frames = 1168
+        self.dataroot       = dataroot
+        self.input_size     = input_size
+
+        # Read data from CSV
+        reader      = csv.reader(open(self.dataroot + "annotations_modified/val.csv", "r"), 
+                                delimiter=",")
+        self.data   = list(reader)
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        # Separate the self.data row its components
+        path        = self.data[index][0]
+        vid_start   = int(self.data[index][-2])
+        vid_end     = int(self.data[index][-1])
+        emotions    = np.array(self.data[index][1:-2], dtype=np.float)
+ 
+        # Read the video using scikit-video library. Takes a lot of time :(
+        vid_array   = skvideo.io.vread(self.dataroot + "videos/" + path, 
+                                        num_frames = vid_end)
+        vid_array   = vid_array[vid_start:]
+        joints      = np.load(self.dataroot + "joints/" + path[:-4] + ".npy")
+        joints      = joints[vid_start:vid_end]
+
+        if (vid_end - vid_start) > self.input_size:
+            # Randomly select frames from the given video of input_size
+            arr         = random.sample(range(vid_end - vid_start), 
                                     self.input_size)
             arr.sort()
             vid_array   = vid_array[arr]
---
+++ tests.py
@@ -0,0 +1,23 @@
+# Rough work file to do code testing
+# TODO Delete the file after the project is over.
+
+from dataloader.loader import *
+import config
+import time
+
+train_loader = BOLDTrainLoader(  dataroot = config.dataset_root, 
+                                input_size = config.input_frames)
+
+val_loader = BOLDValLoader(  dataroot = config.dataset_root, 
+                                input_size = config.input_frames)
+total_time = 0
+for i in range(len(train_loader)):
+    now = time.time()
+    try:
+        x,y,z = train_loader[i]
+    except FileNotFoundError:
+        continue
+    total_time += time.time() - now
+
+print(total_time)
+print(total_time / len(train_loader))
