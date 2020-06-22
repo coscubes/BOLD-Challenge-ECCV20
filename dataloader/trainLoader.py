fbb3d967ef8daf0acc69244d5853b56a18eaabf6
sakhalkark <sakhalkark@gmail.com>
2020-06-22 17:17:52 +0200

    Fixed different frame size issue

--- dataloader/trainLoader.py
+++ dataloader/trainLoader.py
@@ -117,7 +117,7 @@ class BOLDTrainLoader(Dataset):
             frame = vid_array[i][ int(top):int(bottom), int(left):int(right)]
             if frame.shape != (224, 224, 3):
                 frame = cv2.resize(frame, (self.height, self.height))
-                print(vid_height, vid_width, x, y, top, bottom, left, right, frame.shape)
+                print("frame error")
             cropped_vid.append(frame)
             j_frame     = j_frame[2:].reshape(18, 3)
             j_frame     -= np.array([x, y, 0])
