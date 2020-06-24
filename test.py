58b86820053643091c50e68399a648fc9ef07031
adi-dhal <retxham6991@gmail.com>
2020-06-24 04:08:09 +0200

    test

--- test.py
+++ test.py
@@ -42,14 +42,13 @@ model.eval()
 loss = 0
 for i, (vid, joints, emotions) in enumerate(test_loader):
     print(i)
-    vid_collec = vid.cpu().detach().numpy()
-    joints_collec = joints.cpu().detach().numpy()
+    vid_collec = np.squeeze(vid.cpu().detach().numpy())
+    joints_collec = np.squeeze(joints.cpu().detach().numpy())
     #emotions_array = emotions.cpu().detach().numpy()
     emotions= emotions.to(device)
     emotions = emotions.squeeze()
     pred_avg = []
     print(vid_collec.shape,joints_collec.shape)
-    break
     for count in range(config.test_frames):
         vid_tensor = torch.from_numpy(vid_collec[count]).to(device)
         joints_tensor = torch.from_numpy(joints_collec[count]).to(device)
