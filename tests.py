5b87e20a0803cf5f61741b730fc83fbb2ed9b9f0
sakhalkark <sakhalkark@gmail.com>
2020-06-28 03:15:40 +0000

    Changed training and testing mode

--- tests.py
+++ tests.py
@@ -6,26 +6,25 @@ import  torch.nn as nn
 from    torch.optim import Adam
 from    torch.utils.data import DataLoader
 import  torchvision.models as models
-from    torchvision.transforms import *
 
+# Credits to https://github.com/hassony2/torch_videovision for this
 from    torchvideotransforms.video_transforms import *
 from    torchvideotransforms.volume_transforms import *
 from    torchvideotransforms.stack_transforms import *
 
 import  config
-from    dataloader.trainLoader_Skepxles import *
+from    dataloader.trainLoader import *
 from    dataloader.valLoader import *
-import os
 
+# Credits to https://github.com/piergiaj/pytorch-i3d for I3D
+from    models.I3D import InceptionI3d
+
+import os
+import time
+
+# Set the right GPU on server  
 if config.server:
     os.environ["CUDA_VISIBLE_DEVICES"]="3"
-
-if config.device is None:
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-else:
-    device = config.device
-
-print("Using ", device, " for training")
 
 train_transforms    = Compose([
     ColorJitter(0.5, 0.5, 0.25),
@@ -42,24 +41,23 @@ train_data = BOLDTrainLoader(
     dataroot    = config.dataset_root,
     input_size  = config.input_frames,
     height      = config.height,
-    transform   = train_transforms,
-    skep_thresh = 10
+    transform   = train_transforms
 )
-'''
+
 val_data =  BOLDValLoader(
     dataroot    = config.dataset_root,
     input_size  = config.input_frames,
     height      = config.height,
     transform   = val_transforms
 )
-'''
+
 train_loader = DataLoader(
     dataset     = train_data,
     batch_size  = config.batch_size,
     shuffle     = True,
     num_workers = config.num_workers
 )
-'''
+
 val_loader = DataLoader(
     dataset     = val_data,
     batch_size  = config.batch_size,
@@ -67,55 +65,67 @@ val_loader = DataLoader(
     num_workers = config.num_workers
 )
 
-class model(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.vgg = models.vgg11(pretrained=True).features
-        self.avg_pool = nn.AdaptiveAvgPool2d(output_size=(7,7))
-        self.fc1      = nn.Linear(25088, 4096)
-        self.fc2      = nn.Linear(4096, 4096)
-        self.fc3      = nn.Linear(4096, 29)
-        self.act1     = nn.ReLU()
-        self.act2     = nn.ReLU()
-        self.dropout  = nn.Dropout()
+val_loader = DataLoader(
+    val_data,
+    batch_size  = config.batch_size,
+    shuffle     = False,
+    num_workers = config.num_workers
+)
 
-    def forward(self, x):
-        x = self.vgg(x)
-        x = x.flatten(start_dim = 1)
-        x = self.fc1(x)
-        x = self.act1(x)
-        x = self.dropout(x)
-        x = self.fc2(x)
-        x = self.act2(x)
-        x = self.dropout(x)
-        x = self.fc3(x)
-        return x
+if config.device is None:
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+else:
+    device = config.device
 
-net = model().to("cuda")
+print("Using ", device, " for training")
 
-optmizer    = Adam(net.parameters(), lr = config.learning_rate)
 criterion   = torch.nn.MSELoss(reduction='sum')
+model       = InceptionI3d(num_classes=400, in_channels=3, mode=2)
 
-print("we reached here")
-'''
-for i, (vid, joints, emo) in enumerate(train_loader):
-    # vid, joints, emo    = train_data[i]
-    if i % 100 == 0:
-        print("reached", i)
-    # print(vid.shape, joints.shape, emo.shape)
-    # vid                 = np.transpose(vid,(0, 3,1,2))
-    # vid                 = vid.to(device)
-    # print(vid.shape)
-    # emo                 = torch.Tensor(np.ones((16,29)) * emo).to(device)
+model.load_state_dict(torch.load("checkpoints/rgb_imagenet.pt"))
+model.replace_logits(config.logits)
+model.to(device)
+optimizer   = Adam(model.parameters(), lr = config.learning_rate)
 
-    # pred = net(vid)
-    # loss = criterion(pred, emo)
-    # loss.backward()
-    # optmizer.step()
-    # print(loss)
-'''
-for i, (vid, joints, emo) in enumerate(val_loader):
-    # vid, joints, emo    = train_data[i]
-    if i % 100 == 0:
-        print("reached", i)
-'''
+
+# Save an initial fully constructed model
+# torch.save(model, config.model_path + "full_model.pt")
+
+for epoch in range(config.num_epochs):
+    total_train_loss = 0.0
+    total_val_loss   = 0.0
+    epoch_start = time.time()
+    num_iter    = 0
+    print("Starting epoch Num:", (epoch+1))
+    for i, (vid, joints, emotions) in enumerate(train_loader):
+        optimizer.zero_grad()
+        vid     = vid.to(device)
+
+        emotions= emotions.to(device)
+        joints  = joints.to(device)
+        # torch.Size([8, 32, 224, 224, 3])
+        preds   = model(vid)
+        print(preds.shape)
+        break
+        loss    = criterion(preds, emotions)
+        total_train_loss += loss
+        loss.backward()
+        optimizer.step()
+        num_iter += 1
+    print("Train Loss = ", total_train_loss / num_iter)
+    break
+    num_iter = 0
+    
+    with torch.no_grad():
+        for i, (vid, joints, emotions) in enumerate(val_loader):
+            optimizer.zero_grad()
+            vid     = vid.to(device)
+            emotions= emotions.to(device)
+            joints  = joints.to(device)
+            preds   = model(vid)
+            loss    = criterion(preds, emotions)
+            total_val_loss += loss
+            num_iter += 1
+    print("Validations Loss = ", total_val_loss / num_iter)
+    print("Epoch time taken = ", time.time() - epoch_start)
+    # torch.save(model.state_dict(), config.model_path + "model-epoch-" + str(epoch) + ".pt")
